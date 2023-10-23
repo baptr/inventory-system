c749c588c5073c6a51adf6bd31e7ce40b064235f
baptr <1522777+baptr@users.noreply.github.com>
2023-10-23 01:52:36 -0700

    Support deselecting from the hotbar

--- core/hotbar.gd
+++ core/hotbar.gd
@@ -1,26 +1,26 @@
-@tool
 @icon("res://addons/inventory-system/icons/hotbar.svg")
 extends NodeInventorySystemBase
 class_name Hotbar
 
-signal on_change_selection(selection_index)
+signal selection_changed(selection_index)
 
 
 @export var inventory : Inventory
 
 @export var slots_in_hot_bar := 8
 
-var selection_index := 0
+var selection_index := -1
 
 func _ready():
 	super._ready()
 	if inventory != null:
-		inventory.updated_slot.connect(_on_updated_slot.bind())
+		inventory.updated_slot.connect(_on_updated_slot)
 
 
 func change_selection(index : int):
 	if index < 0 or index >= slots_in_hot_bar:
 		return
+	set_selection_index(index)
 
 
 func next_item():
@@ -31,17 +31,23 @@ func previous_item():
 	set_selection_index(selection_index + 1)
 
 
+func select_none():
+	selection_index = -1
+	selection_changed.emit(selection_index)
+
+
 func set_selection_index(new_index : int):
 	if new_index >= slots_in_hot_bar:
 		new_index -= slots_in_hot_bar
 	if new_index < 0:
 		new_index += slots_in_hot_bar
-	selection_index = new_index
-	emit_signal("on_change_selection", selection_index)
+	if selection_index != new_index:
+		selection_index = new_index
+		selection_changed.emit(selection_index)
 
 
 func has_valid_item_id() -> bool:
-	if selection_index >= inventory.slots.size():
+	if selection_index < 0 or selection_index >= inventory.slots.size():
 		return false
 	var slot = inventory.slots[selection_index]
 	if slot == null:
@@ -62,5 +68,6 @@ func get_selected_item() -> InventoryItem:
 
 
 func _on_updated_slot(slot_index : int):
+	# TODO: Why is this necessary?
 	if slot_index == selection_index:
 		set_selection_index(selection_index)
